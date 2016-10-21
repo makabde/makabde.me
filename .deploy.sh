693058a53951575c1f611530b04c311b580b93bd
Mak Abdennabi <hello@makabde.me>
2016-10-21 13:35:25 +0200

    Add script that will deploy once travis-ci pass

--- .deploy.sh
+++ .deploy.sh
@@ -15,3 +15,25 @@ set -e
 
 npm run build
 gulp deploy --env production
+
+# Cleanup
+
+rm -rf ../makabde.github.io
+
+# Clone the GH pages repo using GH_TOKEN for authentification
+
+git clone https://${GH_TOKEN}@github.com/makabde/makabde.github.io ../makabde.github.io
+
+# Copy the generated assets to the master branch
+
+cp -R build/production/* ../makabde.github.io
+
+# Commit and push the geneated content to the master branch
+# Since the repo was cloned in write mode with token auth - we can push there
+
+cd ../makabde.github.io
+git config user.email "hello@makabde.me"
+git config user.name "Mak Abdennabi"
+git add -A .
+git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER"
+git push --quiet origin master > /dev/null 2>&1
